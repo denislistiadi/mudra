da2abcd8c124eaedb5344d660bb8b0ddb0292668
Denis Listiadi <denislistiadi24@gmail.com>
2025-12-17 19:39:07 +0700

    feat: adjust input autocomplete

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
-  export let form;
+	export let form;
 </script>
 
 <h1>Login</h1>
 
 <form method="POST">
-  <div>
-    <!-- svelte-ignore a11y_label_has_associated_control -->
-    <label>Email</label>
-    <input name="email" type="email" required />
-  </div>
+	<div>
+		<!-- svelte-ignore a11y_label_has_associated_control -->
+		<label>Email</label>
+		<input name="email" type="email" autocomplete="email" required />
+	</div>
 
-  <div>
-    <!-- svelte-ignore a11y_label_has_associated_control -->
-    <label>Password</label>
-    <input name="password" type="password" required />
-  </div>
+	<div>
+		<!-- svelte-ignore a11y_label_has_associated_control -->
+		<label>Password</label>
+		<input name="password" type="password" autocomplete="current-password" required />
+	</div>
 
-  {#if form?.error}
-    <p style="color: red">{form.error}</p>
-  {/if}
+	{#if form?.error}
+		<p style="color: red">{form.error}</p>
+	{/if}
 
-  <button type="submit">Login</button>
+	<button type="submit">Login</button>
 </form>
